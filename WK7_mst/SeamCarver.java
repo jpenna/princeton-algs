483ed76f841aeae3c2f93bb61027415f5863d023
Juliano Penna <jpenna@churchcommunitybuilder.com>
2021-02-28 13:12:22 -0300

    wk7 good seams

--- WK7_mst/SeamCarver.java
+++ WK7_mst/SeamCarver.java
@@ -46,39 +46,70 @@ public class SeamCarver {
     return energyCalc.calcEnergy(picture, x, y);
   }
 
+  private void getNext(int coord, boolean isVertical, double[] pathWeights, int[][] paths) {
+    int lengthSkipLast = pathWeights.length - 1;
+
+    // For each path, skipping edges
+    for (int i = 1; i < lengthSkipLast; i++) {
+      // Select the next pixel with the smallest energy
+      double smallestEnergy = Double.POSITIVE_INFINITY;
+      // Use the previous index of the current path to bound the next index
+      int prev = paths[i][coord - 1];
+      int selected = 0;
+      for (int diff = -1; diff <= 1; diff++) {
+        double e = isVertical ? energy(prev + diff, coord) : energy(coord, prev + diff);
+        if (smallestEnergy > e) {
+          smallestEnergy = e;
+          selected = prev + diff;
+        }
+      }
+      // Increase the path weight and pixels
+      pathWeights[i] += smallestEnergy;
+      paths[i][coord] = selected;
+    }
+  }
+
+  private int[] findSeam(boolean isVertical) {
+    int dimension = isVertical ? picture.width() : picture.height();
+    int pathSize = !isVertical ? picture.width() : picture.height();
+
+    double[] pathWeights = new double[dimension];
+    int[][] paths = new int[dimension][pathSize];
+
+    // Disconsider edges
+    for (int i = 1; i < dimension - 1; i++) {
+      paths[i][0] = i;
+      pathWeights[i] = 1000; // starting edge
+    }
+
+    for (int i = 1; i < pathSize; i++)
+      getNext(i, isVertical, pathWeights, paths);
+
+    int smIndex = 1;
+    for (int i = 1; i < pathWeights.length - 1; i++) { // Skip edges
+      if (pathWeights[smIndex] > pathWeights[i]) {
+        smIndex = i;
+      }
+    }
+
+    int[] selected = paths[smIndex];
+    // Last pixel is the same as pixel before last
+    if (selected.length > 1) {
+      selected[selected.length - 1] = selected[selected.length - 2];
+    }
+
+    // StdOut.println("Total weight: " + pathWeights[smIndex]);
+    return selected;
+  }
+
   // sequence of indices for horizontal seam
   public int[] findHorizontalSeam() {
     if (hSeam != null)
       return hSeam;
 
-    return new int[]{};
-  }
+    hSeam = findSeam(false);
 
-  private int getNextY(int y, double[] pathWeight, int[][] paths, int smallestPath) {
-    int smallest = smallestPath;
-
-    // For each column from 1:w-1
-    for (int x = 1; x < picture.width() - 1; x++) {
-      // Select the next pixel with the smallest energy
-      double smallestEnergy = Double.POSITIVE_INFINITY;
-      int selectedX = -1;
-      for (int diff = -1; diff <= 1; diff++) {
-        double e = energy(x + diff, y);
-        if (smallestEnergy > e) {
-          smallestEnergy = e;
-          selectedX = x + diff;
-        }
-      }
-      // Increase the path
-      pathWeight[x-1] += smallestEnergy;
-      paths[x-1][y] = selectedX;
-      // If current path is smaller the the previously selected, choose the current one
-      if (pathWeight[smallest] > pathWeight[x-1]) {
-        smallest = x-1;
-      }
-    }
-
-    return smallest;
+    return hSeam;
   }
 
   // sequence of indices for vertical seam
@@ -86,54 +117,85 @@ public class SeamCarver {
     if (vSeam != null)
       return vSeam;
 
-    // Disconsider edges
-    double[] pathWeight = new double[picture.width() - 2];
-    int[][] paths = new int[pathWeight.length][picture.height()];
+    vSeam = findSeam(true);
 
-    for (int x = 0; x < pathWeight.length; x++) {
-      paths[x][0] = x+1;
-    }
-
-    int smIndex = 0;
-    for (int y = 1; y < picture.height() - 1; y++)
-      smIndex = getNextY(y, pathWeight, paths, smIndex);
-
-    int[] selected = paths[smIndex];
-    // Last pixel is the same as pixel before last
-    selected[selected.length - 1] = selected[selected.length - 2];
-
-    return selected;
+    return vSeam;
   }
 
   // remove horizontal seam from current picture
   public void removeHorizontalSeam(int[] seam) {
     Validation.validHorizontalSeam(picture, seam);
-    // not a valid seam (i.e., either an entry is outside its prescribed range or two adjacent entries differ by more than 1).
+
+    int w = picture.width();
+    int h = picture.height();
+
+    Picture newPicture = new Picture(w, h - 1);
+
+    int prev = 0;
+    for (int x = 0; x < w; x++) {
+      for (int y = 0; y < h; y++) {
+        if (y < prev - 1 || y > prev + 1) {
+          throw new IllegalArgumentException();
+        }
+        prev = y;
+        if (seam[x] == y) continue;
+        newPicture.setRGB(x, y, picture.getRGB(x, y));
+      }
+    }
+
+    picture = newPicture;
     resetCaches();
   }
 
   // remove vertical seam from current picture
   public void removeVerticalSeam(int[] seam) {
     Validation.validVerticalSeam(picture, seam);
-    // not a valid seam (i.e., either an entry is outside its prescribed range or two adjacent entries differ by more than 1).
+
+    int w = picture.width();
+    int h = picture.height();
+
+    Picture newPicture = new Picture(w - 1, h);
+
+    int prev = 0;
+    for (int x = 0; x < w; x++) {
+      for (int y = 0; y < h; y++) {
+        if (x < prev - 1 || x > prev + 1) {
+          throw new IllegalArgumentException();
+        }
+        prev = x;
+        if (seam[y] == x) continue;
+        newPicture.setRGB(x, y, picture.getRGB(x, y));
+      }
+    }
+
+    picture = newPicture;
     resetCaches();
   }
 
   //  unit testing (optional)
   public static void main(String[] args) {
-    Picture pic = new Picture("/Users/jpenna/Documents/princeton-algs/WK7_mst/samples/6x5.png");
+    Picture pic = new Picture("/Users/jpenna/Documents/princeton-algs/WK7_mst/samples/12x10.png");
     SeamCarver sc = new SeamCarver(pic);
 
     // /Users/jpenna/Documents/princeton-algs/WK7_mst/samples/3x4.png
     StdOut.println("Width (3): " + sc.width());
     StdOut.println("Height (4): " + sc.height());
     StdOut.println("Energy 0,0 (1000): " + sc.energy(0, 0));
-    StdOut.println("Energy 1,1 (sqrt 52024 = 228.087702): " + sc.energy(1, 2));
+    // StdOut.println("Energy 1,1 (sqrt 52024 = 228.087702): " + sc.energy(1, 2));
+    StdOut.println("Energy 0,3: " + sc.energy(0, 3));
 
     int[] vSeam = sc.findVerticalSeam();
-    StdOut.println("Vertical Seam: ");
+    StdOut.print("Vertical Seam: ");
     for (int i : vSeam) {
       StdOut.printf("%d, ", i);
     }
+    StdOut.println();
+
+    int[] hSeam = sc.findHorizontalSeam();
+    StdOut.print("Horizontal Seam: ");
+    for (int i : hSeam) {
+      StdOut.printf("%d, ", i);
+    }
+    StdOut.println();
   }
 }
