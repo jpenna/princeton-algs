78ebb7f82a4c7b1914bda6e5620dbbfa3e94e8a0
Juliano Penna <jpenna@churchcommunitybuilder.com>
2021-03-01 11:56:53 -0300

    wk7 from bottom

--- WK7_mst/SeamCarver.java
+++ WK7_mst/SeamCarver.java
@@ -1,3 +1,5 @@
+import edu.princeton.cs.algs4.IndexMinPQ;
+import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.Picture;
 import edu.princeton.cs.algs4.StdOut;
 
@@ -5,12 +7,11 @@ public class SeamCarver {
   private Picture picture;
   private final EnergyCalc energyCalc;
   private double minEnergy;
-  private int[] minPath;
-  private int[] path;
-  private int endScan;
-  private int endPath;
-  private boolean isVertical;
-  private int[] sides = new int[]{-1, 1, 0};
+  private int[][] nextPixelV;
+  private double[][] energyMatrix;
+  private int lowerV;
+  private int w;
+  private int h;
 
   // create a seam carver object based on the given picture
   public SeamCarver(Picture picture) {
@@ -39,66 +40,87 @@ public class SeamCarver {
     Validation.validCoordinates(picture, x, y);
 
     // Edge pixels
-    if (x == 0 || y == 0 || x == picture.width() - 1 || y == picture.height() - 1)
+    if (x == 0 || y == 0 || x == width() - 1 || y == height() - 1)
       return 1000;
 
     return energyCalc.calcEnergy(picture, x, y);
   }
 
-  private void findNext(int scanVal, int pathCursor, double pathEnergy) {
-    // Do not consider paths going through edges
-    if (pathEnergy >= minEnergy) {
-      return;
-    }
-    if (pathCursor == endPath) {
-      minEnergy = pathEnergy; // minEnergy will be smaller
-      minPath = path.clone();
-      return;
-    }
-    if (pathCursor > endPath) {
-      throw new IllegalArgumentException("pathCursor should never be bigger than height/width, because it should stop earlier.");
-    }
-    for (int side : sides) {
-      int scanIndex = scanVal+side;
+  private int getIndex(int x, int y) {
+    return x + y*w;
+  }
 
-      if (scanIndex >= endScan || scanIndex < 1) continue;
+  private int[] getCoords(int index) {
+    return new int[]{ index % w, index / w };
+  }
 
-      int x = isVertical ? scanIndex : pathCursor;
-      int y = isVertical ? pathCursor : scanIndex;
-      path[pathCursor] = scanIndex;
-
-      findNext(scanIndex, pathCursor+1, pathEnergy + energy(x, y));
+  private void setEnergyV(int x, int y, int nextX, int nextY) {
+    double treeEnergy = energyMatrix[nextX][nextY];
+    double e = energy(x, y) + treeEnergy;
+    if (e < energyMatrix[x][y]) {
+      energyMatrix[x][y] = e;
+      nextPixelV[x][y] = getIndex(nextX, nextY);
+      if (y == 0 && e < energyMatrix[lowerV][0]) {
+        lowerV = x;
+      }
     }
   }
 
-  private int[] findSeam() {
-    int w = picture.width();
-    int h = picture.height();
+  private void processNextV(int x, int y, Queue<Integer> queue) {
+    int yAbove = y - 1;
+    if (yAbove < 0) return;
 
-    endScan = isVertical ? w : h;
-    endPath = isVertical ? h : w;
-
-    path = new int[endPath];
-    minPath = new int[endPath];
-    minEnergy = Double.POSITIVE_INFINITY;
-
-    for (int i = 2; i < endScan - 1; i += 3) {
-      findNext(i, 0, 0);
+    for (int slide = -1; slide <= 1; slide++) {
+      if (x+slide < 1 || x+slide >= w-1) {
+        continue;
+      }
+      int index = getIndex(x + slide, yAbove);
+      queue.enqueue(index);
+      setEnergyV(x + slide, yAbove, x, y);
     }
-
-    return minPath;
-  }
-
-  // sequence of indices for horizontal seam
-  public int[] findHorizontalSeam() {
-    isVertical = false;
-    return findSeam();
   }
 
   // sequence of indices for vertical seam
   public int[] findVerticalSeam() {
-    isVertical = true;
-    return findSeam();
+    w = width();
+    h = height();
+    nextPixelV = new int[w][h];
+    energyMatrix = new double[w][h];
+    Queue<Integer> queue = new Queue<>();
+    for (int i = 0; i < w; i++) {
+      for (int j = 0; j < h; j++) {
+        nextPixelV[i][j] = -1;
+        energyMatrix[i][j] = Double.POSITIVE_INFINITY;
+      }
+    }
+    energyMatrix[0][h - 1] = 0; // bottom-left pixel is fake...
+
+    for (int col = 2; col < w; col += 2) {
+      int y = h-1;
+      queue.enqueue(getIndex(col, y));
+      setEnergyV(col, y, 0, y);
+      nextPixelV[col][y] = -1;
+
+      while(!queue.isEmpty()) {
+        int index = queue.dequeue();
+        int[] coords = getCoords(index);
+        processNextV(coords[0], coords[1], queue);
+      }
+    }
+
+    int[] seam = new int[h];
+    int next = lowerV;
+    for (int i = 0; i < h; i++) {
+      seam[i] = next;
+      next = getCoords(nextPixelV[next][i])[0];
+    }
+
+    return seam;
+  }
+
+  // sequence of indices for horizontal seam
+  public int[] findHorizontalSeam() {
+    return new int[]{};
   }
 
   // remove horizontal seam from current picture
@@ -180,11 +202,11 @@ public class SeamCarver {
     }
     StdOut.println();
 
-    int[] hSeam = sc.findHorizontalSeam();
-    StdOut.printf("Horizontal Seam (%f): ", sc.minEnergy);
-    for (int i : hSeam) {
-      StdOut.printf("%d, ", i);
-    }
-    StdOut.println();
+    // int[] hSeam = sc.findHorizontalSeam();
+    // StdOut.printf("Horizontal Seam (%f): ", sc.minEnergy);
+    // for (int i : hSeam) {
+    //   StdOut.printf("%d, ", i);
+    // }
+    // StdOut.println();
   }
 }
