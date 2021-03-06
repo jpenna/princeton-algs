3f20f461ffcddb92c3a883f9ba8d61d81ccfe23e
Juliano Penna <jpenna@churchcommunitybuilder.com>
2021-03-06 16:40:02 -0300

    wk8 build digraph

--- WK8_maxflow/BaseballElimination.java
+++ WK8_maxflow/BaseballElimination.java
@@ -4,16 +4,15 @@ import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.DirectedEdge;
 import edu.princeton.cs.algs4.EdgeWeightedDigraph;
 import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.StdOut;
 
 // The last six methods should throw an IllegalArgumentException if one (or both) of the input arguments are invalid teams.
 
 class BaseballElimination {
   private Team[] teams;
-  private int[] games;
   private int firstVirtual;
   private int lastVirtual;
   private int leaderId;
+  private int maxFlow;
 
   // create a baseball division from given filename in format specified below
   public BaseballElimination(String filename) {
@@ -23,23 +22,20 @@ class BaseballElimination {
     firstVirtual = teamsNum;
     lastVirtual = teamsNum + 1;
     teams = new Team[teamsNum];
-    games = new int[(teamsNum * teamsNum - teamsNum) / 2];
 
     leaderId = 0;
-    int id = 0;
-    int gameN = 0;
-    while (id < teamsNum) {
+    for (int id = 0; id < teamsNum; id++) {
       String name = in.readString();
       int wins = in.readInt();
 
-      teams[id] = new Team(id, name, wins, in.readInt(), in.readInt());
+      int[] games = new int[teamsNum];
+      for (int i = 0; i < teamsNum; i++) {
+        games[i] = in.readInt();
+      }
+
+      teams[id] = new Team(id, name, wins, in.readInt(), in.readInt(), games);
 
       if (teams[leaderId].wins < wins) leaderId = id;
-
-      int[] gamesList = in.readAllInts();
-      for (int i = id + 1; i < teamsNum; i++) {
-        games[gameN++] = gamesList[i];
-      }
     }
   }
 
@@ -49,13 +45,15 @@ class BaseballElimination {
     private int wins;
     private int losses;
     private int left;
+    private int[] games;
 
-    public Team(int id, String name, int wins, int losses, int left) {
+    public Team(int id, String name, int wins, int losses, int left, int[] games) {
       this.id = id;
       this.name = name;
       this.wins = wins;
       this.losses = losses;
       this.left = left;
+      this.games = games;
     }
   }
 
@@ -101,36 +99,55 @@ class BaseballElimination {
   public int against(String team1, String team2) {
     Team a = findTeam(team1);
     Team b = findTeam(team2);
-    return games[a.id * teams.length + b.id];
+    return a.games[b.id];
+  }
+
+  private EdgeWeightedDigraph buildDigraph(Team current) {
+    int n = teams.length;
+    /* n: teams
+     * (n*n - n) / 2: number of unique games
+     * 2: virtual nodes */
+    EdgeWeightedDigraph digraph = new EdgeWeightedDigraph(n + ((n*n - n)/2) + 2);
+    // Games index start
+    int g = lastVirtual + 1;
+    int bestScore = current.wins + current.left;
+    maxFlow = 0;
+
+    for (int id = 0; id < teams.length; id++) {
+      Team team = teams[id];
+      if (team == current) continue;
+
+      digraph.addEdge(new DirectedEdge(team.id, lastVirtual, bestScore));
+
+      // id+1 to disregard game with self and skip previously added nodes
+      for (int opponent = id+1; opponent < teams.length; opponent++) {
+        int gameNode = g++;
+        maxFlow += team.games[opponent];
+        digraph.addEdge(new DirectedEdge(firstVirtual, gameNode, team.games[opponent]));
+        digraph.addEdge(new DirectedEdge(gameNode, team.id, Double.POSITIVE_INFINITY));
+        digraph.addEdge(new DirectedEdge(gameNode, opponent, Double.POSITIVE_INFINITY));
+      }
+    }
+
+    return digraph;
   }
 
   // is given team eliminated?
   public boolean isEliminated(String name) {
     Team current = findTeam(name);
+    // Leader is not eliminated
+    if (current.id == leaderId) {
+      return false;
+    }
+
     // Trivial elimination
     if (current.wins + current.left < teams[leaderId].wins) {
       return true;
     }
 
-    int n = teams.length;
-    // TODO Optimization: (n-1)-1 nodes won't be used because the current team is disregarded
-    EdgeWeightedDigraph digraph = new EdgeWeightedDigraph(n + games.length + 2);
+    EdgeWeightedDigraph digraph = buildDigraph(current);
 
-    for (int game = 0; game < n; game++) {
-      games[game]
-    }
-
-
-    for (int teamId = 0; teamId < n; teamId++) {
-      if (teams[teamId] == current) continue;
-
-      int base = teamId * n;
-      for (int i = base; i < n; i++) {
-        DirectedEdge edge = new DirectedEdge(firstVirtual, base + i, games[i]);
-      }
-      DirectedEdge edge = new DirectedEdge(team, opponent, in.readInt());
-      digraph.addEdge(edge);
-    }
+    return true;
   }
 
   // subset R of teams that eliminates given team; null if not eliminated
@@ -139,7 +156,9 @@ class BaseballElimination {
   }
 
   public static void main(String[] args) {
-    BaseballElimination division = new BaseballElimination(args[0]);
+    String file = "/Users/jpenna/Documents/princeton-algs/WK8_maxflow/samples/teams4.txt";
+    BaseballElimination division = new BaseballElimination(file);
+    // BaseballElimination division = new BaseballElimination(args[0]);
     for (String team : division.teams()) {
       if (division.isEliminated(team)) {
         StdOut.print(team + " is eliminated by the subset R = { ");
